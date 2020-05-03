58ee4f0e69c4acc42a9ba890fd4f6c0d2a0e8873
Yura Husak <yura.husak@juriba.com>
2020-05-03 15:32:05 +0300

    Simple http server

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const chalk = require('chalk');
 const text = require("./data");
+const http = require('http');
 
-console.log(chalk.blue(text.helloText));
+const server = http.createServer((req, res) => {
+	res.end(text.helloText);
+});
+
+server.listen(3000, () => {
+	console.log("Server has been started...");
+});
